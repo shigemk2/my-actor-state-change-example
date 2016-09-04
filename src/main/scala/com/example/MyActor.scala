78910f78a149d4b60879b793a2fece5bd29245e4
shigemk2 <i.am.shige@gmail.com>
2016-09-04 22:14:35 +0900

    Add angry

--- src/main/scala/com/example/MyActor.scala
+++ src/main/scala/com/example/MyActor.scala
@@ -1,5 +1,24 @@
 package com.example
 
-class MyActor {
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.actor.Actor
+import akka.event.Logging
 
+class MyActor extends Actor {
+  val log = Logging(context.system, this)
+
+  def angry: Receive = {
+    case "be angry" => {
+      log.info("already angry")
+    }
+    case "be happy" => {
+      log.info("angry -> happy")
+      context.become(happy)
+    }
+    case _ => {
+      log.info("unbecome angry")
+      context.become
+    }
+  }
 }
